ee0408d22975816116107cd5093bb67b09bc69e0
s2485523800 <43655924@qq.com>
2023-11-06 14:07:04 +0800

    fix excelModel

--- src/main/java/com/disaster/disastercode/DisasterCodeApplication.java
+++ src/main/java/com/disaster/disastercode/DisasterCodeApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan("com.disaster.disastercode.mapper")
 @EnableAspectJAutoProxy
+@EnableScheduling
 public class DisasterCodeApplication {
 
     public static void main(String[] args) {
